79df1047c1082a07db07dd9b8d4b2e1f82fc28dc
Francesco Romani <fromani@redhat.com>
2020-09-17 10:58:11 +0200

    numacell: even more debug output
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/numacell/main.go
+++ cmd/numacell/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/fromanirh/k8s-device-plugins/pkg/numacell"
 	"github.com/golang/glog"
 	"github.com/kubevirt/device-plugin-manager/pkg/dpm"
@@ -12,7 +13,7 @@ func main() {
 	flag.Parse()
 
 	devs, _ := numacell.GetNUMACellDevices("/sys") // TODO
-	glog.Infof("detected: %+#v", devs)
+	glog.Infof("detected: %s", spew.Sdump(devs))
 
 	manager := dpm.NewManager(numacell.NUMACellLister{})
 	manager.Run()
